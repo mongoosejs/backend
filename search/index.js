23c9033746b9f4efc558435fab417fc6e69e2446
Valeri Karpov <val@karpov.io>
2021-05-14 16:54:43 -0400

    chore: switch to azure

---
+++ search/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const cheerio = require('cheerio');
+const config = require('../.config');
+const mongoose = require('mongoose');
+
+let conn = null;
+const contentSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  url: { type: String, required: true }
+});
+contentSchema.index({ title: 'text', body: 'text' });
+
+module.exports = async function search(context, req) {
+  let Content;
+  if (conn == null) {
+    conn = mongoose.createConnection(config.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await conn;
+
+    Content = conn.model('Content', contentSchema, 'Content');
+  }
+
+  const $search = req.query.search.toString();
+  let results = await Content.
+    find({ $text: { $search } }, { score: { $meta: 'textScore' } }).
+    sort({ score: { $meta: 'textScore' } }).
+    limit(10);
+
+  results = results.map(doc => {
+    const $ = cheerio.load(doc.body);
+
+    doc.body = $('p').get(0) ? $('p').first().html() : doc.body;
+
+    return doc;
+  });
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: JSON.stringify({ results })
+  };
+}
