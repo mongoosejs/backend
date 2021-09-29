0bfaa7710fc3595260cb4707daf536be980c62b8
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2021-09-29 18:06:07 -0400

    add

---
+++ test.js
@@ -0,0 +1,36 @@
+
+const axios = require('axios');
+const mongoose = require('mongoose');
+const slackToken = 'xoxb-80341368871-2550790998148-Mbj9RlvHoIJuffemEB0asPDP';
+const subscriberSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    githubUsername: { type: String, required: true },
+    githubUserId: { type: String },
+    githubOrganization: { type: String },
+    githubOrganizationId: { type: String }
+  });
+  
+ run().catch(err => console.log(err));
+
+async function run() {
+  const url = 'https://slack.com/api/chat.postMessage';
+  const res = await axios.post(url, {
+    channel: '#pro-notifications',
+    text: 'Hello, World!'
+  }, { headers: { authorization: `Bearer ${slackToken}` } });
+
+  console.log('Done', res.data);
+}
+
+// test();
+
+async function test() {
+    let conn = mongoose.createConnection('mongodb+srv://mongoose21:ggknyTbObfxa6kF7@cluster0-xvnqv.mongodb.net/mongoose?retryWrites=true&w=majority');
+    await conn.asPromise();
+   let Subscriber = conn.model('Subscriber', subscriberSchema, 'Subscriber');
+   await Subscriber.create({
+       email: 'dcd57@miami.edu',
+       githubUsername: 'IslandRhythms'
+   });
+   console.log('done');
+}
