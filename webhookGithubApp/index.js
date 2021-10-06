e02f6aa925dcf23226374d6703280155bb34d8ab
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2021-10-06 17:49:45 -0400

    Just need to plug in the private stuff and you should be good to go

--- webhookGithubApp/index.js
+++ webhookGithubApp/index.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 const azureWrapper = require('../util/azureWrapper');
-const { createTokenAuth } = require('@octokit/auth-token');
+const { createAppAuth } = require('@octokit/auth-token');
 const config = require('../.config');
 const mongoose = require('mongoose');
 
@@ -25,16 +25,25 @@ module.exports = azureWrapper(async function webhookGithubApp(context, req) {
 
   Subscriber = conn.model('Subscriber', subscriberSchema, 'Subscriber');
 
-  // const { token } = await createTokenAuth(config.githubAccessTokenForMongoose)();
-
   const { installation, sender } = req.body;
+  const auth = createAppAuth({
+    id: ,
+    privateKey: ,
+    installationId: installation.id,
+    clientId: ,
+    clientSecret: 
+  });
+  const { token } = await auth({ type: 'installation' });
 
   if(installation.account.type == 'Organization') {
-    const membersList =  await axios.get(`https://api.github.com/orgs/${installation.account.login}/members`).then((res) => res.data);
+    const membersList =  await axios.get(`https://api.github.com/orgs/${installation.account.login}/members`, { headers: {
+      authorization: `bearer ${token}`
+    }}).then((res) => res.data);
     const memberOrg = installation.account.login;
     const orgId = installation.account.id;
     for (let i = 0; i < membersList.length; i++) {
       let userInfo = await axios.get(`https://api.github.com/users/${membersList[i].login}`);
+      if(await Subscriber.find({email: userInfo.email})) continue;
       await Subscriber.create({
         email: userInfo.email,
         githubUsername: membersList[i].login,
