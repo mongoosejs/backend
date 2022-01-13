541b5481f0e2bc53bec05d8ff2d0f087d153c672
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2022-01-13 11:02:42 -0500

    made adjustments

--- createChannel.js
+++ createChannel.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const {WebClient} = require('@slack/web-api');
-const config = require('./.config/.config.js');
+const config = require('./.config/development.js')
 const readline = require('readline');
 const r1 = readline.createInterface({
     input: process.stdin,
@@ -11,10 +11,12 @@ const r1 = readline.createInterface({
  run().catch(err => console.log(err));
 
 async function run() {
-    const client = new WebClient(config.userSlackToken); // Use the User OAuth Token, not Bot User Oauth Token
+    const client = new WebClient(config.slackToken);
     let res = await client.conversations.list({types: 'public_channel, private_channel'});
     res = res.channels.map((item) => item.name_normalized);
     console.log(res);
+    const val = await client.users.lookupByEmail({email: 'val@karpov.io'});
+    const daniel = await client.users.lookupByEmail({email: 'daniel@meanitsoftware.com'});
     r1.question('What is the name of the channel you wish to create? ', function(name) {
         if(res.includes(name.toLowerCase())) {
             r1.emit('close', 'Name is already taken, please try again and enter a unique name.');
@@ -28,8 +30,11 @@ async function run() {
             } else if(status.toLowerCase() != 'public' && status.toLowerCase() != 'private') {
                 r1.emit('close', 'Not a valid option. please try again and enter public or private.');
             } 
-            let response = await client.conversations.create({name: name, is_private: status });
+            let response = await client.conversations.create({name: name.toLowerCase(), is_private: status });
             console.log(response);
+            let reservation = await client.conversations.invite({channel: response.channel.id , users: `${val.user.id}, ${daniel.user.id}`});
+            console.log(response);
+            r1.emit('close', `Channel created and invites sent to ${val.user.real_name} and ${daniel.user.real_name}!`);
         });
     });
 }
