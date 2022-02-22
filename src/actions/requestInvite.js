5faabf18c98e2466a2c96bb91f178d7e5140ae92
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2022-02-22 15:20:52 -0500

    Create requestInvite.js

---
+++ src/actions/requestInvite.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+async function requestInvite(email) {
+    const url = 'https://slack.com/api/chat.postMessage';
+    await axios.post(url, {
+      channel: '#pro-notifications',
+      blocks: [
+        {type: 'divider'},
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `${email} is requesting access to the mongoosejs workspace`
+          }
+        },
+      ]
+    }, { headers: { authorization: `Bearer ${config.slackToken}` } });
+}
