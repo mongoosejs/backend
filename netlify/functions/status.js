65640b42fea262c87eedc7e567ce624a0132eedf
Valeri Karpov <val@karpov.io>
2024-12-18 17:06:25 -0500

    import fix

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const extrovert = require('extrovert');
-const pkg = require('../package.json');
+const pkg = require('../../package.json');
 
 module.exports = extrovert.toNetlifyFunction(async function status() {
   return { ok: 1, version: pkg.version, nodeVersion: process.version };
